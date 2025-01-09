45fd4284e90c3ea3baf1c1b382d53fa56cb702f2
josdic1 <joshuadicker@gmail.com>
2025-01-09 14:31:05 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const recipients = ["Guadalupe", "Ollie", "Aki"];
+const prefix = "Thank you";
+const suffix = "for the wonderful surprise gift!";
+
+function writeCards(arr) {
+   const array = arr.map(item => (
+      `${prefix}, ${item}, ${suffix}`
+   ))
+   return array
+}
+
+function countDown(num) {
+   for(let i = 0; i <= num; i++) {
+      console.log(i)
+   }
+}
+
+countDown(10)
+
+
