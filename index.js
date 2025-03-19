40b0e8c2f90ecff674aaeb9c85eced016c2c43d1
Josh Dicker <joshdicker@joshs-mac-mini.mynetworksettings.com>
2025-03-19 09:54:30 -0400

    loopalong done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solutions in this file
+function countDown(end) {
+    while (end >= 0) {
+      console.log(end);
+      end--;
+    }
+  }
+
+  function writeCards(names, occasion) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${occasion} gift!`);
+  }
